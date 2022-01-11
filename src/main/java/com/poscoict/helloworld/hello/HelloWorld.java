2abc53f5ad9911a36bef06ab8fbeca450a3069f6
BIT <BIT@DESKTOP-28SDCPC>
2022-01-11 17:10:32 +0900

    test commit

--- src/main/java/com/poscoict/helloworld/hello/HelloWorld.java
+++ src/main/java/com/poscoict/helloworld/hello/HelloWorld.java
@@ -1,6 +1,6 @@
 package com.poscoict.helloworld.hello;
 
-public class HelloWorld {//메이븐으로 되어있기 때문에 빌드 가능
+public class HelloWorld {//메이븐으로 되어있기 때문에 빌드 가능-> 최종 결과물이 jar 파일로 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
